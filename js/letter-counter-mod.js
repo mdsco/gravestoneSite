d308d19e05f118cede607de638fe7aed6eb615a2
mike <scoboria@gmail.com>
2017-03-07 18:11:13 -0500

    Updating price on product page on the fly when text added, modified or deleted

--- js/letter-counter-mod.js
+++ js/letter-counter-mod.js
@@ -2,6 +2,11 @@
 
 	$( window ).load( function() {	
 
+		const pricePerChar = 5.00;
+		const basePrice = 25.00;
+
+		$(".price > .woocommerce-Price-amount").html('');
+
 		$this = $(this);
 
 		var current_item_key = 'no_key';
@@ -11,6 +16,7 @@
 		var getCorrectCharacterCount = function(){
 
 			var session = sessionStorage.getItem('wc_fragments');
+
 			if(session !== null){
 				var key = session.match(/cart_item_key(.*?)\\/g);
 
@@ -26,8 +32,34 @@
 
 				}
 			}
+			
+			var count = getCountOfCurrentlyDisplayedChars();
 
-			// verify FPD has any view instances
+			$.ajax({
+
+			    global: false,
+			    type: "POST",
+			    cache: false,
+			    dataType: "json",
+			    data: ({
+			        action: 'write',
+			        char_count : count,
+			        product_key : current_item_key
+			    }),
+			    url: '/wordpress/wp-content/plugins/marksPlugin/get-letter-count.php',
+			    success: function(response){
+					console.log("Response: ", response);
+				},
+				error: function(error){
+					console.log("Error: ", error);
+				}
+
+			});
+
+		}
+
+		var getCountOfCurrentlyDisplayedChars = function(){
+
 			if(fancyProductDesigner.currentViewInstance !== null){
 
 				var count = 0;
@@ -42,7 +74,8 @@
 						text += fancyProductDesigner.currentViewInstance.stage._objects[i].text;
 
 						// extract non alpha-numeric characters
-						text = text.replace(/[^a-zA-Z0-9]/g, '');
+						// text = text.replace(/[^a-zA-Z0-9]/g, '');
+						text = text.replace(/[\s]/g, '');
 
 						//get character count
 						count = text.length;
@@ -51,47 +84,59 @@
 
 				}
 
-				$.ajax({
-
-				    global: false,
-				    type: "POST",
-				    cache: false,
-				    dataType: "json",
-				    data: ({
-				        action: 'write',
-				        char_count : count,
-				        product_key : current_item_key
-				    }),
-				    url: '/wordpress/wp-content/plugins/marksPlugin/get-letter-count.php',
-				    success: function(response){
-						console.log("Response: ", response);
-					},
-					error: function(error){
-						console.log("Error: ", error);
-					}
-				});
+				return count;
 
 			}
 
+			return 0;
+
+		}
+
+		var updateProductPagePrice = function(){
+
+			if(fancyProductDesigner.currentViewInstance !== null){
+
+				var charCount = getCountOfCurrentlyDisplayedChars();
+
+				var total = basePrice + (charCount * pricePerChar);
+
+				var userLang = navigator.language || navigator.userLanguage;
+
+				var currency = '';
+
+				if(userLang == 'en-US'){
+					currency = '$';	
+				} else if(userLang == 'en-GB'){
+					currency = '&pound;';
+				}
+
+				var dollarString = currency + total + ".00";
+
+				$(".price > .woocommerce-Price-amount").html(dollarString);
+			} 
 		}
 
 		$this.on('productCreate', function(){
 			getCorrectCharacterCount();
+			updateProductPagePrice();
 		});
 
 		$this.on('elementModify', function(){
 			getCorrectCharacterCount();
+			updateProductPagePrice();
 
 		});
 
 		// update character count on elementAdd
 		$this.on('elementAdd', function(){
 			getCorrectCharacterCount();
+			updateProductPagePrice();
 		});
 
 		// update character count on elementDelete
 		$this.on('elementRemove', function(){
 			getCorrectCharacterCount();
+			updateProductPagePrice();
 		});
 
 		$( document ).on('click', 'td.product-remove > a', function( event ){
